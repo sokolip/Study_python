a38d80a6801994ae45b7d7ef1941446ab7fb2502
sokolip <sokolip8@gmail.com>
2023-12-08 10:56:54 -0500

    08.12

---
+++ stepik_python/int.py
@@ -0,0 +1,25 @@
+print(11111*1111111)
+
+print(40//8)
+
+print(42%8)
+
+print(2014**14)
+
+print(7//3)
+
+print(int(-1.6))
+
+print(9**19 -int(float(9**19)))
+
+a = 3
+
+print(a)
+
+name = input('Enter your name')
+print ("Hello", name)
+
+a = int(input())
+print(a*2)
+
+
