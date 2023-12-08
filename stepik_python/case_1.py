94ac15f94e160d2baa0bd1888e9b4a792266adda
sokolip <sokolip8@gmail.com>
2023-12-08 13:49:57 -0500

    86.12

--- stepik_python/case_1.py
+++ stepik_python/case_1.py
@@ -8,10 +8,19 @@
 # print(int(x / 60))
 # print(int(x % 60))
 
-X = int(input()) #минут для сна
-H = int(input())
-M = int(input())
-X1 = H * 60 + M
-X2 = X1 + X
-print(int(X2/60))
-print(int(X2%60))
+# X = int(input()) #минут для сна
+# H = int(input())
+# M = int(input())
+# X1 = H * 60 + M
+# X2 = X1 + X
+# print(int(X2/60))
+# print(int(X2%60))
+
+
+x = 5
+y = 10
+print(y > x * x or y >= 2 * x and x < y)
+
+a = True
+b = False
+print(a and b or not a and not b)
